9b886efb8830fdd6953716453a9787e1110b6ad2
Stoyan Stoyanov <s.stoyanoff@gmail.com>
2023-11-30 09:45:35 +0200

    update

--- remote_exec/main.tf
+++ remote_exec/main.tf
@@ -1,3 +1,18 @@
+variable "vm_ip" {}
+variable "username" {}
+# variable "timezone" {}
+# variable "new_hostname" {}
+variable "private_key_name" {
+  type        = string
+  description = "File path of private key."
+  default     = "id_rsa"
+}
+variable "key_path"{
+  type        = string
+  description = "Dir path of private key."
+  default     = "/home/stoyan/.ssh/it"
+}
+
 resource "null_resource" "remote_vm" {
     connection {
         type = "ssh"
@@ -5,17 +20,14 @@ resource "null_resource" "remote_vm" {
         private_key = "${file("${var.key_path}/${var.private_key_name}")}"
         # The connection will use the local SSH agent for authentication.
         agent = false
-        timeout = "2m"
+        timeout = "1m"
         host = "${var.vm_ip}"
     }
 
 provisioner "remote-exec" {
+    # INSTALL K3S
     inline=[
-      # INSTALL K3S
-      "curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=1.26.10+k3s2 INSTALL_K3S_EXEC= "--disable local-storage --disable servicelb --disable traefik"
-      
-      
-       sh -",
-
+"curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.27.7+k3s2 INSTALL_K3S_EXEC='--disable local-storage --disable servicelb --disable traefik --token=q1w2e3r4100@ --tls-san=ubuntu-test-stoyan.finonex.com' sh -"
     ]
+    }
 }
